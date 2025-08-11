ed823c29ee84a8d459c7a55c4eeb2c7bbfeff18b
Reaper1241 <freddddddddd854@gmail.com>
2025-08-11 17:15:30 +0300

    card

--- server/routes/feeds/[filename].ts
+++ server/routes/feeds/[filename].ts
@@ -1,9 +1,11 @@
-import { defineEventHandler, sendStream } from 'h3'
+import { defineEventHandler, sendStream, setHeader } from 'h3'
 import { createReadStream } from 'node:fs'
 import { join } from 'pathe'
-
+ 
 export default defineEventHandler((event) => {
   const { filename } = event.context.params
   const filePath = join(process.cwd(), 'runtime_public/feeds', filename)
+ 
+  setHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
   return sendStream(event, createReadStream(filePath))
 })
